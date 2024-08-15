34748df02cbe3471945505c5a3b346e0c443de20
Ayesha Shahzaib <146367193+AyeshaShahzaib@users.noreply.github.com>
2024-08-15 19:21:38 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,23 @@
+import styles from "./page.module.css";
+
+export default function Hello() {
+  return (
+    <div className={styles.main_div}>
+      <div className={styles.div}>
+    <div >
+     <h1 className={styles.Heading}>First Milestone Assignment of Next.js</h1>
+    </div>
+    </div>
+      <div className={styles.div}>
+    <div className={styles.Div1}>
+     <h1 className={styles.h1}>Hello World</h1>
+    </div>
+    </div>
+    <div className={styles.div}>
+    <div className={styles.Div2}>
+     <h2 className={styles.h2}>By Ayesha Shahzaib</h2>
+    </div>
+    </div>
+    </div>
+  );
+}
